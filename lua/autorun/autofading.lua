5519a9fd7d135d53ccdec9e733851fccbf532ab1
VLTcorp <36853441+VLTcorp@users.noreply.github.com>
2022-01-21 23:06:57 +0100

    1er push

--- lua/autorun/autofading.lua
+++ lua/autorun/autofading.lua
@@ -1,19 +1,30 @@
 
-local autofading = CreateConVar("cc_autofading", "1" )
-local kolizion = CreateConVar("cc_nocollide", "1" )
-local npcverrif = CreateConVar("cc_only_npc_ragdoll", "0" )
-local autofadingdur = CreateConVar("cc_autofading_time", "120" )
+local autofading = CreateConVar("ccc_autofading", "1" )
+local kolizion = CreateConVar("ccc_nocollide", "1" )
+local npcverrif = CreateConVar("ccc_all_ragdoll", "" )
+local autofadingdur = CreateConVar("ccc_autofading_time", "120" )
 
 --le code du bistro
 if SERVER then 
 
-    hook.Add("OnEntityCreated", "otophadingue" , function (ent)        
+    hook.Add("OnEntityCreated", "clean_ragdoll_vlt" , function (ent)        
         if ent:IsRagdoll() and (  IsValid(ent:GetOwner()) or npcverrif:GetBool() ) then
             if kolizion:GetBool() then
                 ent:SetCollisionGroup(COLLISION_GROUP_WEAPON)
             end
             if autofading:GetBool() then                
                 ent:Fire("FadeAndRemove", "", autofadingdur:GetFloat())
+            end
+        end
+    end)
+
+    hook.Add("CreateEntityRagdoll", "clean_corpses_vlt" , function (owner,ragdoll)        
+        if (  IsValid(ragdoll:GetOwner()) or npcverrif:GetBool() ) then
+            if kolizion:GetBool() then
+                ragdoll:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+            end
+            if autofading:GetBool() then                
+                ragdoll:Fire("FadeAndRemove", "", autofadingdur:GetFloat())
             end
         end
     end)
@@ -26,8 +37,8 @@ else --code du consomateur
             panel:ClearControls()
             panel:CheckBox("auto fading",autofading:GetName() )
             panel:NumSlider( "Fading time", autofadingdur:GetName() , 0, 900 )
-            panel:CheckBox("corpse collide",kolizion:GetName() )
-            panel:CheckBox("apply only for the npc ragdoll",npcverrif:GetName())      
+            panel:CheckBox("corpse no collide",kolizion:GetName() )
+            panel:CheckBox("apply for all ragdolls",npcverrif:GetName())      
         end )
     end )
 
